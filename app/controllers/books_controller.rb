8a13bf79f1c96f1548bbbf55c0d641e7c979fbe7
kn2728 <csj.chemistry728@gmail.com>
2021-01-24 13:22:51 +0900

    画像登録

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,10 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
