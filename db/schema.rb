2583f890ffa0ebe5854c865e54a016d8ea4af664
kn2728 <csj.chemistry728@gmail.com>
2021-02-07 16:35:09 +0900

    booksテーブルisbn追加user_id消去

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_30_042959) do
+ActiveRecord::Schema.define(version: 2021_02_07_073121) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -36,10 +36,9 @@ ActiveRecord::Schema.define(version: 2021_01_30_042959) do
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
     t.string "author", null: false
-    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_books_on_user_id"
+    t.integer "isbn"
   end
 
   create_table "memos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -66,7 +65,6 @@ ActiveRecord::Schema.define(version: 2021_01_30_042959) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "books", "users"
   add_foreign_key "memos", "books"
   add_foreign_key "memos", "users"
 end
