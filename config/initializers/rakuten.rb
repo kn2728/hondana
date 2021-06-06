23a579fbe8e6d6e421c0671e47ec886242ecc401
kn2728 <csj.chemistry728@gmail.com>
2021-06-06 16:06:41 +0900

    アプリケーションID　変更

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,6 +1,6 @@
 RakutenWebService.configure do |c|
   # (必須) アプリケーションID
-  c.application_id = ENV['APPRICATION_ID']
+  c.application_id = '1027697410333068131'
 
   # (任意) 楽天アフィリエイトID
   c.affiliate_id = ENV['AFFILIATE_ID']
