cf0834c927cfdc0991c8e241cfbf3199bb120f82
kn2728 <csj.chemistry728@gmail.com>
2021-06-16 11:22:57 +0900

    feat:不要記述削除

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -15,8 +15,6 @@ import VueRouter from 'vue-router'
 
 import BookIndexPage from 'BookIndexPage.vue'
 import BookShowPage from 'BookShowPage.vue'
-import BookNewPage from 'BookNewPage.vue'
-import BookEditPage from 'BookEditPage.vue'
 import BookSearchPage from 'BookSearchPage.vue'
 import SummaryNewPage from 'SummaryNewPage.vue'
 import SummaryIndexPage from 'SummaryIndexPage.vue'
@@ -31,23 +29,17 @@ const router = new VueRouter({
     { path: '/books/:id(\\d+)',  // :id は数値のみに制限する
       name: 'BookShowPage',  // ルートに名前を付けている ref. https://router.vuejs.org/ja/guide/essentials/named-routes.html#%E5%90%8D%E5%89%8D%E4%BB%98%E3%81%8D%E3%83%AB%E3%83%BC%E3%83%88
       component: BookShowPage },
-    { path: '/books/new',
-      name: 'BookNewPage',
-      component: BookNewPage },
-    { path: '/books/:id(\\d+)/edit',
-      name: 'BookEditPage',
-      component: BookEditPage   },
     { path: '/books/search',
       name: 'BookSearchPage',
       component: BookSearchPage   },
-    { path: '/books/:id(\\d+)/summaries/new',  // :id は数値のみに制限する
-      name: 'SummaryNewPage',  // ルートに名前を付けている ref. https://router.vuejs.org/ja/guide/essentials/named-routes.html#%E5%90%8D%E5%89%8D%E4%BB%98%E3%81%8D%E3%83%AB%E3%83%BC%E3%83%88
+    { path: '/books/:id(\\d+)/summaries/new',  
+      name: 'SummaryNewPage',  
       component: SummaryNewPage },
-    { path: '/books/:id(\\d+)/summaries',  // :id は数値のみに制限する
-      name: 'SummaryIndexPage',  // ルートに名前を付けている ref. https://router.vuejs.org/ja/guide/essentials/named-routes.html#%E5%90%8D%E5%89%8D%E4%BB%98%E3%81%8D%E3%83%AB%E3%83%BC%E3%83%88
+    { path: '/books/:id(\\d+)/summaries',  
+      name: 'SummaryIndexPage',  
       component: SummaryIndexPage },
-    { path: '/books/:book_id(\\d+)/summaries/:id(\\d+)/edit',  // :id は数値のみに制限する
-      name: 'SummaryEditPage',  // ルートに名前を付けている ref. https://router.vuejs.org/ja/guide/essentials/named-routes.html#%E5%90%8D%E5%89%8D%E4%BB%98%E3%81%8D%E3%83%AB%E3%83%BC%E3%83%88
+    { path: '/books/:book_id(\\d+)/summaries/:id(\\d+)/edit',  
+      name: 'SummaryEditPage',  
       component: SummaryEditPage },
   ]
 })
